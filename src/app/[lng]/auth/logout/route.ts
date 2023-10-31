fcb80aaf6d2b3dcfacfcf1b641f4190cb465fa34
ricepotato <sukjun40@naver.com>
2023-10-31 21:12:16 +0900

    logout route

--- src/app/[lng]/auth/logout/route.ts
+++ src/app/[lng]/auth/logout/route.ts
@@ -2,6 +2,17 @@ import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export async function GET() {
-  cookies().delete("next-auth.session-token");
+  await logout();
+}
+
+async function logout() {
+  const nextAuthCookieNames = [
+    "next-auth.session-token",
+    "next-auth.csrf-token",
+    "next-auth.callback-url",
+  ];
+  nextAuthCookieNames.forEach((name) => {
+    cookies().delete(name);
+  });
   redirect("/");
 }
